00e09913e6de57ca4b5637d12903e5611ab126b4
“ankita” <ankita@example.com>
2020-03-27 18:10:54 +0530

    some changes

--- spec/features/create_spec.rb
+++ spec/features/create_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 RSpec.feature "post", :type => :feature do
   scenario "Create a new post" do
+   expect(FactoryBot.build(:post)).to be_valid
     visit "/posts/new"
     fill_in "Title", :with => "ruby"
     fill_in "Body", :with => "ruby on rails"
     click_button "Create Post"
-    expect(page).to have_text("ruby")
-    expect(page).to have_text("ruby on rails")
+   expect(build(:post)).to be_valid
   end
 end
