374ce92e074a0c8a5f30cd0241cbede2411ecba3
“ankita” <ankita@example.com>
2020-03-27 14:30:51 +0530

    some changes

--- spec/features/edit_spec.rb
+++ spec/features/edit_spec.rb
@@ -1,16 +1,15 @@
-require 'rails_helper'
-require 'spec_helper'
 
+require 'rails_helper'
 RSpec.feature "post", :type => :feature do
-	  let!(:post) { create :post, title: 'hello'}
-	  describe 'Update post' do
+	let!(:post) { create :post, title: 'hello'}
+	describe 'Update post' do
 		scenario 'with valid input' do
-	    visit edit_post_path(post)
-	    fill_in 'Title', with: 'good morning'
-	    click_on 'Update Post'
-	    expect(page).to have_content('good morning')
-	    expect(page).not_to have_content('hello')
-    end
+		  visit edit_post_path(post)
+		  fill_in 'Title', with: 'good morning'
+		  click_on 'Update Post'
+		  expect(page).to have_content('good morning')
+		  expect(page).not_to have_content('hello')
+	  end
   	scenario 'with invalid input' do
 		  visit edit_post_path(post)
 			fill_in 'Title', with: ''
@@ -18,4 +17,4 @@ RSpec.feature "post", :type => :feature do
 			expect(page).to have_content('Title can not be blank')
     end
   end
-  end
+end
